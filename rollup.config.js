d1ceceb783d640dd5a0a81e488199cde39d465a9
ordinaryask <ordinaryask@gmail.com>
2022-07-29 18:36:17 +0200

    feat: add rollup

---
+++ rollup.config.js
@@ -0,0 +1,48 @@
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import typescript from '@rollup/plugin-typescript'
+import dts from 'rollup-plugin-dts'
+import { terser } from 'rollup-plugin-terser'
+import peerDepsExternal from 'rollup-plugin-peer-deps-external'
+import url from '@rollup/plugin-url'
+import css from 'rollup-plugin-import-css'
+import external from 'rollup-plugin-node-externals'
+import postcss from 'rollup-plugin-postcss'
+
+const packageJson = require('./package.json')
+
+export default [
+  {
+    input: 'lib/index.ts',
+    output: [
+      {
+        file: packageJson.main,
+        format: 'cjs',
+        sourcemap: true,
+      },
+      {
+        file: packageJson.module,
+        format: 'esm',
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      peerDepsExternal(),
+      external(),
+      resolve(),
+      commonjs(),
+      typescript({ tsconfig: './tsconfig.json' }),
+      postcss(),
+      terser(),
+      url(),
+      css(),
+    ],
+    external: ['react', 'react-dom', 'styled-components'],
+  },
+  {
+    input: 'dist/esm/index.d.ts',
+    output: [{ file: 'dist/index.d.ts', format: 'esm' }],
+    external: [/\.css$/],
+    plugins: [dts()],
+  },
+]
