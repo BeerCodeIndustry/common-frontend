4b5ebff2bfaaca09774fcabea44b667c8ba1e012
ordinaryask <ordinaryask@gmail.com>
2022-10-24 23:32:45 +0200

    feat: change colors type

--- src/lib/core/Modal/Modal.types.ts
+++ src/lib/core/Modal/Modal.types.ts
@@ -1,12 +1,12 @@
 import { ReactNode } from 'react'
 
-import { Color, ColorWithOpacity, ModalSize } from '../../theme'
+import { AnyColor, ModalSize } from '../../theme'
 
 export interface ModalProps extends React.HTMLAttributes<HTMLElement> {
   size: ModalSize
-  modalColor: Color | ColorWithOpacity
-  backgroundColor: Color | ColorWithOpacity
+  modalColor: AnyColor
+  backgroundColor: AnyColor
   children: ReactNode
   onClose: () => void
-  crossColor?: Color | ColorWithOpacity
+  crossColor?: AnyColor
 }
